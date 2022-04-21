bbdfed7b5725bd87198cfefb32aead028108fc6c
niiyazobug <niiyaz5555@gmail.com>
2022-04-21 12:36:18 +0600

    get user in database

--- index.js
+++ index.js
@@ -4,23 +4,39 @@ const port = process.env.PORT || 5000
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
 // use middleware
 app.use(cors())
 app.use(express.json())
 // -----------------------
 
-// mongodb database setup
-// username : dbuser1
-// password: MTzzgQK6gxZPT4QU
 
-const uri = "mongodb+srv://dbuser1:MTzzgQK6gxZPT4QU@cluster0.uhi0o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// usernme: neyaz
+// pass:8oty2v2x2ZA3KZ7Z
+
+
+const uri = "mongodb+srv://neyaz:8oty2v2x2ZA3KZ7Z@cluster0.l30j2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("foodExpress").collection("users");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
+
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+
+        // create a document to insert
+        const user = { name: 'Devid Dan', email: 'david@gmail.com' }
+
+
+        const result = await userCollection.insertOne(user);
+        console.log(`User inserted with _id: ${result.insertedId}`);
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir)
+
 
 
 app.get('/', (req, res) => {
