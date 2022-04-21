bbaadcc3277d419ccc1ca9bae4d31145b2f4b348
niiyazobug <niiyaz5555@gmail.com>
2022-04-21 12:57:07 +0600

    note added

---
+++ __steps.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * 1. create a node server with 5 steps
+ * 1.1 - create folder
+ * 1.2 - npm init
+ * 1.3 - npm install express cors mongodb
+ * 1.4 - script-dev : nodemon index.js
+ * 1.5 - create index.js
+ * 1.6 - use 5 steps to create a node server
+ * 
+ * ------------------------------------------
+ * 
+ * Create Mongodb Atlas Account
+ * -----------------------------
+ * 1. sign up in mongodb
+ * 2. create cluster
+ * 3. create user neyaz and pass
+ * 4. Network access --> IP address: current IP and allow all (0.0.0.0.)
+ * 5. database > Connect > code copy from CRUD mongo website and paste in index.js
+ * ---------------------------------
+ * 
+ * CRUD Operation
+ * ---------------------------------
+ * 1. node mongodb CRUD > Fundamentals
+ * 2. create async run function
+ * 
+*/
