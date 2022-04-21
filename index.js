931e48b66a6a1c00dfe60ae5a35efd7bbb4c4244
niiyazobug <niiyaz5555@gmail.com>
2022-04-21 22:35:58 +0600

    update profile method added

--- index.js
+++ index.js
@@ -39,12 +39,35 @@ async function run() {
             res.send(users)
         })
 
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await userCollection.findOne(query)
+            res.send(result)
+        })
+
         // POST User : add a new user
         app.post('/user', async (req, res) => {
             const newUser = req.body
             console.log('adding new user', newUser)
             const result = await userCollection.insertOne(newUser)
             res.send(result)
+        })
+
+        // update user
+        app.put('/user/:id', async (req, res) => {
+            const id = req.params.id
+            const updatedUser = req.body
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                }
+            }
+            const resut = await userCollection.updateOne(filter, updatedDoc, options)
+            res.send(resut)
         })
 
         // delete a user
